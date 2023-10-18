674a14620bb99d7c13d67103176cbd5fee46b332
gabrielgws.dev <gabrielgws.dev@gmail.com>
2023-10-18 15:53:09 -0300

    feat: add Footer components

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="bg-accent px-4 py-4 text-[0.623rem] opacity-75">
+      © 2023 Copyright <span className="font-semibold">FSW Store</span>
+    </div>
+  )
+}
