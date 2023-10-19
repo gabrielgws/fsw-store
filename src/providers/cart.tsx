613b7ac89ccb49fa4bbad282d3a8abe95310ac70
gabrielgws.dev <gabrielgws.dev@gmail.com>
2023-10-19 10:53:53 -0300

    feat: ensure product quantity can be modified on cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -13,6 +13,7 @@ interface ICartContext {
   cartBasePrice: number;
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -20,7 +21,8 @@ export const CartContext = createContext<ICartContext>({
   cartTotalDiscount: 0,
   cartBasePrice: 0,
   cartTotalPrice: 0,
-  addProductToCart: () => {}
+  addProductToCart: () => {},
+  decreaseProductQuantity: () => {}
 })
 
 const CartProvider = ({children}: {children: ReactNode}) => {
@@ -50,10 +52,24 @@ const CartProvider = ({children}: {children: ReactNode}) => {
     setProducts((prev) => [...prev, product])
   }
 
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts(prev => prev.map(cartProduct => {
+      if (cartProduct.id === productId) {
+        return {
+          ...cartProduct,
+          quantity: cartProduct.quantity - 1 
+        }
+      }
+        return cartProduct
+      }).filter((cartProduct) => cartProduct.quantity > 0)
+    )
+  }
+
   return (
     <CartContext.Provider value={{
       products,
       addProductToCart,
+      decreaseProductQuantity,
       cartTotalDiscount: 0,
       cartBasePrice: 0,
       cartTotalPrice: 0,
